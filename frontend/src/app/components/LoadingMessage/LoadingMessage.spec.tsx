243b89a9b9432e11233a9a6109ae77b71496b31b
amgmoh <amgmoh94@gmail.com>
2022-07-14 19:07:13 +0200

    fixed

--- frontend/src/app/components/LoadingMessage/LoadingMessage.spec.tsx
+++ frontend/src/app/components/LoadingMessage/LoadingMessage.spec.tsx
@@ -8,6 +8,8 @@ describe('<LoadingMessage>', () => {
       it('Should render the props message', () => {
         const message = 'Hello!';
         const wrapper = shallow(<LoadingMessage message={message} />);
-        expect(wrapper.contains(<span>{message}</span>)).toBeTruthy();
+        expect(wrapper.contains(<span>{message}</span>)).toBeTruthy(); 
+      });
+    });
   });
 });
